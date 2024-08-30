cc6cd367e6ebf5a61efca7fd29055320a1b7cf80
Nicole <nsalama@itba.edu.ar>
2024-08-30 16:37:34 -0300

    feat: pipes between app and slaves

--- main.c
+++ main.c
@@ -2,49 +2,75 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/select.h>
+#include <string.h>
 
 #define     SLAVE_QTY       5
-#define     PERCENTAJE_INITIAL  0.1
+#define     PERCENTAJE_INITIAL  1
+
+void sendFile (int pipe_fd, char *arg);
+void setup_pipes(int pipes[SLAVE_QTY][2][2]);
 
 int main(int argc, char *argv[]) {
+    int pipes[SLAVE_QTY][2][2];
     int pid;
     int total_files = argc - 1;
-    printf("total files: %d\n", total_files);
-
     int initial_files_qty = total_files * PERCENTAJE_INITIAL; 
-    printf("initial files: %d\n", initial_files_qty);
-
     int files_per_slave = initial_files_qty / SLAVE_QTY > 0 ? initial_files_qty / SLAVE_QTY : 1;
-    printf("files per slave: %d\n", files_per_slave);
-
     int remaining_files = total_files - files_per_slave * SLAVE_QTY;
-    printf("remaining files: %d \n", remaining_files);
 
+    printf("total files: %d\n", total_files);
+    printf("initial files: %d\n", initial_files_qty);
+    printf("files per slave: %d\n", files_per_slave);
+    printf("remaining files: %d\n", remaining_files);
+
+    setup_pipes(pipes);
+    
     int current_index = 1; // indice del archivo a procesar
 
-    for(int i = 0; i < SLAVE_QTY; i++){
+    for(int i = 0; i < SLAVE_QTY ; i++){
         pid = fork();
 
         if(pid < 0) {
             perror("Error al crear el proceso");
             return 1;
-        } else if (pid == 0) {
-            // Este es el proceso hijo, se ejecuta el codigo del hijo
-            int start_index = i * files_per_slave + 1;
-            int end_index = start_index + files_per_slave;
+        } else if (pid == 0) { // Este es el proceso hijo
+            // pipes[i][0] de main a slave
+            // pipes[i][1] de slave a main
+            close(pipes[i][0][1]); 
+            close(pipes[i][1][0]);
 
-            //char *args[] = {"./slave", argv[i], NULL };
-            char *args[files_per_slave + 2];
-            args[0] = "./slave";
-            for (int j = start_index, k = 1; j < end_index; j++, k++) {
-                args[k] = argv[j];
-            }
-            args[files_per_slave + 1] = NULL;
+            dup2(pipes[i][0][0], STDIN_FILENO);
+            close(pipes[i][0][0]);
 
+            dup2(pipes[i][1][1], STDOUT_FILENO);
+            close(pipes[i][1][1]);
+
+            char *args[] = {"./slave", NULL};
             execve(args[0], args, NULL);
             perror("Error al ejecutar el proceso hijo"); // si execve falla
-            exit(1); // Salir si execve falla
+            exit(EXIT_FAILURE); // Salir si execve falla
+        }else if  (pid > 0){ // Proceso padre
+            /*close(pipes[i][0][0]);
+            close(pipes[i][1][1]);
+
+            dup2(pipes[i][0][1], STDOUT_FILENO);
+            close(pipes[i][0][1]);
+
+            dup2(pipes[i][1][0], STDIN_FILENO);
+            close(pipes[i][1][0]);*/
         }
+    }
+
+    for (int i = 0; i < SLAVE_QTY; i++) {
+        for (int j = 0; j < files_per_slave; j++) {
+            if (current_index <= total_files) {
+                sendFile(pipes[i][0][1], argv[current_index]);
+                printf("%s\n", argv[current_index]);
+                current_index++;
+            }
+        }
+        //close(pipes[i][0][1]);
     }
 
     for (int i = 0; i < SLAVE_QTY; i++) {
@@ -52,4 +78,24 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
+}
+
+void setup_pipes(int pipes[SLAVE_QTY][2][2]) {
+    for (int i = 0; i < SLAVE_QTY; i++) {
+        if (pipe(pipes[i][0]) == -1) {  // Pipe main->slave
+            perror("Error al crear el pipe main->slave");
+            exit(EXIT_FAILURE);
+        }
+        if (pipe(pipes[i][1]) == -1) {  // Pipe slave->main
+            perror("Error al crear el pipe slave->main");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void sendFile(int pipe_fd, char *arg) {
+    if (write(pipe_fd, arg, strlen(arg) + 1) == -1) {
+        perror("Error al escribir en el pipe");
+        exit(1);
+    }
 }
--- slave.c
+++ slave.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc ; i++)
-    {
-        printf("Soy el slave con pid %d procesando %s \n", getpid(), argv[i]);
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+    if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        printf("Recibido por slave con pid %d: %s\n", getpid(), buffer);
+    } else if (bytes_read == -1) {
+        perror("Error al leer de stdin");
+        exit(EXIT_FAILURE);
     }
+
+    printf("execve realizado\n");
+    
     
     return 0;
 }
