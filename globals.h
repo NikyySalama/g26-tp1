d093b54936d4d0d07f78088d91b8bc059c43f33b
Valentin <valentin.garfi@gmail.com>
2024-08-30 16:16:16 -0300

    feat: made basic shared memory space between processes

---
+++ globals.h
@@ -0,0 +1,2 @@
+#define SEMAPHORE_NAME   "/application_view_semaphore"
+#define SHARED_MEMORY_NAME "/application_view_shared_memory"
--- view.c
+++ view.c
@@ -1,45 +1,35 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "globals.h"
-#include <fcntl.h>  // Biblioteca necesaria para shm_open
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>  // Biblioteca necesaria para shm_open
 #include <sys/mman.h>  // Biblioteca necesaria para mmap y shm_open
+#include <unistd.h>  // Biblioteca necesaria para ftruncate
 #include <sys/stat.h>
+#include "globals.h"
 
 #define SHARED_MEMORY_PERMISSIONS   0666
 #define SHARED_MEMORY_SIZE          4096
 
-#define     SLAVE_QTY       5
+void* startSharedMemory(void);
+void endSharedMemory(void* ptr);
 
-void viewTesting(void);
-int main(int argc, char *argv[]) {
-    viewTesting();
-    // int pid;
-    // for(int i = 1; i <= SLAVE_QTY; i++){
-    //     pid = fork();
-
-    //     if(pid < 0) {
-    //         perror("Error al crear el proceso");
-    //         return 1;
-    //     } else if (pid == 0) {
-    //         // Este es el proceso hijo, se ejecuta el codigo del hijo
-    //         char *args[] = {"./slave", argv[i], NULL };
-    //         execve(args[0], args, NULL);
-    //         perror("Error al ejecutar el proceso hijo"); // si execve falla
-    //         exit(1); // Salir si execve falla
-    //     }
-    // }
+int main(int argc, char const *argv[])
+{
+    void* shm_view_ptr = startSharedMemory();
+    printf("Todo correctamente inicializado\n");
+    // sprintf(shm_view_ptr, "Hola a todos!\n");
+    printf("Lo que hay en el bloque compartido es: %s", (char*) shm_view_ptr);
+    endSharedMemory(shm_view_ptr);
     return 0;
 }
 
-void viewTesting(void) {
+void* startSharedMemory(void) {
     int shm_fd = shm_open(SHARED_MEMORY_NAME, O_CREAT | O_RDWR, SHARED_MEMORY_PERMISSIONS);
     if (shm_fd == -1) {
-        perror("Error al abrir el bloque de memoria compartida con view");
+        // TODO coordinar que hacemos ante error
+        perror("Error al abrir el bloque de memoria compartida con main");
         exit(EXIT_FAILURE);
     }
 
@@ -54,10 +44,18 @@ void viewTesting(void) {
 		exit(EXIT_FAILURE);
 	}
 
-    sprintf(ptr, "Vamos a dejar esto por parte de main\n");
+    return ptr;
+}
 
+void endSharedMemory (void* ptr) {
     if (munmap(ptr, SHARED_MEMORY_SIZE) == -1) {
-        perror("Error al desmapear la memoria compartida por parte del main");
+        perror("Error al desmapear la memoria compartida");
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO coordinar qué proceso hace esto
+    if (shm_unlink(SHARED_MEMORY_NAME) == -1) {
+        perror("Error al eliminar el objeto de memoria compartida");
         exit(EXIT_FAILURE);
     }
 }
