8380b921977718dc4750695d9202c00aaf81f167
Nicole <nsalama@itba.edu.ar>
2024-08-31 11:03:21 -0300

    feat: slave sends signal through pipe

--- main.c
+++ main.c
@@ -6,20 +6,37 @@
 #include <string.h>
 
 #define     SLAVE_QTY           5
-#define     PERCENTAJE_INITIAL  1
+#define     PERCENTAJE_INITIAL  0.1
 #define     W_END               1
 #define     R_END               0
 
 void sendFile (int pipe_fd, char *arg);
+void sendFiles(int pipe_fd, char *arg[], int qty);
 void setup_pipes(int pipes[SLAVE_QTY][2][2]);
+
+struct slaveInfo {
+    int pipe[2][2];
+    int filesToProcess;
+};
 
 int main(int argc, char *argv[]) {
     int pipes[SLAVE_QTY][2][2];
     int pid;
     int total_files = argc - 1;
-    int initial_files_qty = total_files * PERCENTAJE_INITIAL; 
-    int files_per_slave = initial_files_qty / SLAVE_QTY > 0 ? initial_files_qty / SLAVE_QTY : 1;
-    int remaining_files = total_files - files_per_slave * SLAVE_QTY;
+
+    if(total_files == 0){
+        printf("no se ingresaron archivos a procesar\n");
+        return 1;
+    }
+
+    int initial_files_qty = total_files * PERCENTAJE_INITIAL;
+    if (initial_files_qty == 0)
+    {
+        initial_files_qty = SLAVE_QTY;
+    }
+
+    int files_per_slave = initial_files_qty / SLAVE_QTY;
+    int remaining_files = total_files - initial_files_qty;
 
     printf("total files: %d\n", total_files);
     printf("initial files: %d\n", initial_files_qty);
@@ -30,7 +47,7 @@ int main(int argc, char *argv[]) {
     
     int current_index = 1; // indice del archivo a procesar
 
-    for(int i = 0; i < SLAVE_QTY ; i++){
+    for(int i = 0; i < SLAVE_QTY && i < initial_files_qty ; i++){
         pid = fork();
 
         if(pid < 0) {
@@ -56,11 +73,12 @@ int main(int argc, char *argv[]) {
             perror("Error al ejecutar el proceso hijo"); // si execve falla
             exit(EXIT_FAILURE); // Salir si execve falla
         }
-         // Proceso padre
+        // Proceso padre
         close(pipes[i][0][R_END]);
         close(pipes[i][1][W_END]);
     }
 
+    // se envian los archivos iniciales
     for (int i = 0; i < SLAVE_QTY; i++) {
         for (int j = 0; j < files_per_slave; j++) {
             if (current_index <= total_files) {
@@ -69,10 +87,13 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-
-    for (int i = 0; i < SLAVE_QTY; i++) {
-        wait(NULL);
-    }
+    /*while (remaining_files > 0)
+    {
+        if (se recibe DONE por STDIN)
+        {
+            enviarle un nuevo archivo al slave que mando DONE
+        }
+    }*/
 
     return 0;
 }
@@ -97,3 +118,10 @@ void sendFile(int pipe_fd, char *arg) {
     }
     write(pipe_fd, "\n", 1);
 }
+
+/* void sendFiles(int pipe_fd, char *arg[], int qty){
+    for (int i = 0; i < qty; i++)
+    {
+        sendFile(pipe_fd, arg[i]);
+    }
+} no uso por ahora*/
--- slave.c
+++ slave.c
@@ -1,3 +1,5 @@
+
+//slave
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,15 +10,20 @@ int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read;
 
-    printf("execve realizado\n");
-
-    bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+    /*bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
     if (bytes_read > 0) {
         buffer[bytes_read] = '\0';
-        printf("Recibido por slave con pid %d: %s\n", getpid(), buffer);
+        //printf("Recibido por slave con pid %d: %s\n", getpid(), buffer);
     } else if (bytes_read == -1) {
         perror("Error al leer de stdin");
         exit(EXIT_FAILURE);
-    }    
+    }    */
+    while (read(STDIN_FILENO, buffer, sizeof(buffer)) )
+    {
+        // procesa los archivos que se mandan por STDIN
+        // termina de procesarlos y manda por STDOUT una senal de que termino
+        printf("DONE\n"); //senal
+    }
+    
     return 0;
 }
